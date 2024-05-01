95fbd6f0f108c56f32b48b95ff9b9d5d0df8169b
juanvalno <133802373+juanvalno@users.noreply.github.com>
2024-05-01 15:41:56 +0700

    Update streamlit.py

--- Streamlit/streamlit.py
+++ Streamlit/streamlit.py
@@ -6,19 +6,9 @@ from scipy.special import boxcox1p
 from sklearn.preprocessing import PolynomialFeatures
 import requests
 
-url_lamda = 'https://github.com/juanvalno/itb/blob/8bc8de18f3b928da315a94cca326cffd3ad13b29/Model/lambda_values.pkl?raw=true'
-response_lamda = requests.get(url_lamda)
-with open('lambda_values.pkl', 'wb') as f:
-    f.write(response_lamda.content)
-    
-# Load the lambda values
 lambda_values = pickle.load(open('lambda_values.pkl', 'rb'))
 
 # Load the model and other data
-url_model = 'https://github.com/juanvalno/itb/blob/6e27ef37b0dd92209b5760528d026f80e8b955e7/Model/model_lgbm_tune.pkl'
-response_model = requests.get(url_model)
-with open('model_lgbm_tune.pkl', 'wb') as f:
-    f.write(response_model.content)
 model = pickle.load(open('model_lgbm_tune.pkl', 'rb'))
 
 # Extract the model object
